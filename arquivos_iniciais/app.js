e70a1a72066210fe4cecf756c696143ada9209de
LUIZ SILVA <117494851+LuizSilva0205@users.noreply.github.com>
2023-07-10 23:38:11 -0300

    UP

--- arquivos_iniciais/app.js
+++ arquivos_iniciais/app.js
@@ -40,6 +40,27 @@ class Bd {
 
 		localStorage.setItem('id', id)
 	}
+
+	recuperarTodos_osRegistros(){
+
+		let despesas = Array()
+
+		let id = localStorage.getItem('id')
+		for(let i =1; i<=id; i++){
+			let despesa = JSON.parse(localStorage.getItem(i))
+			
+			if( despesa ===null){
+				continue
+			}
+
+			despesas.push(despesa)
+		}
+		return despesas
+	}
+
+	pesquisar(despesa){
+		console.log(despesa)
+	}
 }
 
 let bd = new Bd()
@@ -66,9 +87,75 @@ function cadastrarDespesa() {
 	if(despesa.validarDados()) {
 		bd.gravar(despesa)
 		//dialog de sucesso
-		$('#sucessoGravacao').modal('show') 
+		$('#modalRegistraDespesa').modal('show') 
+		document.getElementById('modal_titulo').innerHTML ='Registro inserido com sucesso'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada corretamente'
+		document.getElementById('modal_btn').innerHTML = 'Voltar'
+		document.getElementById('modal_btn').className = 'btn btn-success'
+		ano.value = ''
+		mes.value = ''
+		dia.value = ''
+		tipo.value =''
+		descricao.value = ''
+		valor.value = ''
 	} else {
-		//dialog de sucesso
-		$('#erroGravacao').modal('show') 
+		//dialog de erro
+		document.getElementById('modal_titulo').innerHTML ='Erro na inclusão do registro'
+
+		document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+
+
+		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação da despesa'
+		document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+		document.getElementById('modal_btn').className = 'btn btn-danger'
+
+		$('#modalRegistraDespesa').modal('show') 
+		
 	}
+
+	
+}
+
+function carregaListaDespesas(){
+	let despesas = Array()
+	despesas = bd.recuperarTodos_osRegistros()
+	
+	let lista_despesas = document.getElementById('listaDespesas')
+
+	//percorrer o array despesas
+	despesas.forEach(function(d){
+
+		
+		let linha = lista_despesas.insertRow()
+
+		linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+		switch(parseInt(d.tipo)){
+			case 1:d.tipo = 'Alimentação'
+			break
+			case 2: d.tipo = 'Educação'
+			break
+			case 3: d.tipo = 'Lazer'
+			break
+			case 4: d.tipo = 'Saúde'
+			break
+			case 5: d.tipo = 'Transporte'
+		}
+		linha.insertCell(1).innerHTML = d.tipo
+		linha.insertCell(2).innerHTML = d.descricao
+		linha.insertCell(3).innerHTML = d.valor
+	})
+}
+
+
+function pesquisarDespesas(){
+	let ano = document.getElementById('ano').value
+	let mes = document.getElementById('mes').value
+	let dia = document.getElementById('dia').value
+	let tipo = document.getElementById('tipo').value
+	let descricao = document.getElementById('descricao').value
+	let valor = document.getElementById('valor').value
+
+	let despesa = new Despesa(ano, mes,dia,tipo,descricao,valor)
+	bd.pesquisar(despesa)
 }
